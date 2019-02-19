0dbfd079008c2e3e634b964176294d6d411dcd99
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-02-20 08:56:36 +1100

    Added copy plugin, which will copy the files in /public to /build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path'); 
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js', 
   output: {
     filename: 'main.js', 
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'public'),
+        to: path.resolve(__dirname, 'build')
+      }
+    ])
+  ]
 };
