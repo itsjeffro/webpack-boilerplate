f1e03fe8c22aee0325fe9544ddce17d3955389d3
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-02-20 13:10:05 +1100

    Added dev server configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/main.js', 
+  entry: './src/main.js',
   output: {
-    filename: 'main.js', 
+    filename: 'main.js',
     path: path.resolve(__dirname, 'build')
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new CopyPlugin([
       {
         from: path.resolve(__dirname, 'public'),
