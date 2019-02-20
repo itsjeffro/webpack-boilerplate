1e508d10a4321e7041b3f30d7cff870c07c7c7f2
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-02-20 12:48:16 +1100

    wip

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-document.getElementById('root').innerHTML = "Hello, webpack!";
+let root = "root";
+let html = "Hello, webpack! There is ES2015+ support.";
+let items = ["apple", "banana", "orange"];
+
+items.map(item => {
+  console.log(item);
+});
+
+document.getElementById(root).innerHTML = html;
