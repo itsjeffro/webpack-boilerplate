2fd8d350265b2d0a5c509638cdd136cd810fb09f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-29 16:25:58 +1100

    Added example App components

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface AppProps {}
+
+class App extends React.Component<AppProps> {
+  render() {
+    return (
+      <>
+        React awesomeness!
+      </>
+    )
+  }
+}
+
+export default App;
